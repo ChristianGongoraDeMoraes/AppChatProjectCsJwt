60e3c4253980331c4c5d309ee6b29475924d7ed0
chris <christianpimpo304@hotmail.com>
2025-05-09 14:40:21 -0300

    .

--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00e174c5cc874ea5dff18a6d36b344f26fd1fce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d11a50d35b4545b89c3a514a791d1844248c54b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
